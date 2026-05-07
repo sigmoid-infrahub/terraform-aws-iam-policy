f752342508a7ad5989c98fd9a0c0996b91a97c03
Juan Lee <me@juany.kr>
2026-05-08 03:04:01 +0900

    Update IAM policy description handling to allow null values

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_policy" "this" {
   name        = var.name
-  description = var.description
+  description = var.description != "" ? var.description : null
   policy      = var.policy_document
   path        = var.path
   tags        = local.resolved_tags
